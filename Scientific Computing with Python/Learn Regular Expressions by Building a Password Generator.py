964be814d40f3eb5edf21fc089f4bf1bf7191eb1
Hyerim JI <rimirimijj@gmail.com>
2024-07-11 10:41:54 +0900

    FCC_~step11

--- Scientific Computing with Python/Learn Regular Expressions by Building a Password Generator.py
+++ Scientific Computing with Python/Learn Regular Expressions by Building a Password Generator.py
@@ -11,15 +11,22 @@ symbols = string.punctuation
 all_characters = letters + digits + symbols
 
 print(all_characters)
-print(random.random())
+
 """
 Step 9 
 returns a floating point number in the range 
 between 0.0 (inclusive) and 1.0 (exclusive).
-ex)) print(random.random())
+ex)) print(random.random()) #0.6309669497757615
 
 Step10
 The choice() function from the random module takes 
 a sequence and returns a random item of the sequence.
+ex)) print(random.choice(all_characters)) #임의 숫자 하나 발생
+
+Step11
+렌덤은 예측이 가능하다. 
+instead of importing random, import the secrets module. 
+Then change the print() call to use secrets.choice(all_characters).
+
 
 """
